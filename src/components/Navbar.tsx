068a8d844fdd7cb9ea9da21da167591c23939263
srdtrk <srdtrk@hotmail.com>
2023-11-29 23:10:08 +0300

    feat: added new empty tab

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ConnectWallet from "./ConnectWallet";
+import { useRouter } from 'next/router';
 
-type Props = {};
+const Navbar = () => {
+  const router = useRouter();
 
-const Navbar = (props: Props) => {
+  const navigateTo = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <div className='bg-gray-100'>
       <div className='container mx-auto flex justify-between items-center p-2'>
-        <h1>Counter Example</h1>
+        <div>
+          <button onClick={() => navigateTo('/')} className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mx-1">Counter Example</button>
+          <button onClick={() => navigateTo('/Console')} className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mx-1">My DApp</button>
+        </div>
         <ConnectWallet />
       </div>
     </div>
---
+++ src/pages/Console.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Console = () => {
+  return (
+    <div>
+      <h1>My DApp Console</h1>
+      {/* Your DApp content goes here */}
+    </div>
+  );
+};
+
+export default Console;
