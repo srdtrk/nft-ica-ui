cc9fdfda234f277b6a429c0793951c05db313967
srdtrk <srdtrk@hotmail.com>
2023-12-15 03:29:18 +0300

    style: improved navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ConnectWallet from './ConnectWallet'
 import { useRouter } from 'next/router'
 
+const debugMode = false
+
 const Navbar = (): JSX.Element => {
   const router = useRouter()
 
@@ -12,24 +14,28 @@ const Navbar = (): JSX.Element => {
   return (
     <div className="bg-gray-100 shadow-md">
       <div className="container mx-auto flex justify-between items-center p-2">
-        <div>
-          <button
-            onClick={() => {
-              navigateTo('/')
-            }}
-            className="font-bold py-2 px-4 rounded mx-1 border-solid border-2 border-black hover:border-gray-500 hover:bg-gray-200 hover:text-gray-500"
-          >
-            NFT ICA App
-          </button>
-          <button
-            onClick={() => {
-              navigateTo('/Console')
-            }}
-            className="font-bold py-2 px-4 rounded mx-1 border-solid border-2 border-black hover:border-gray-500 hover:bg-gray-200 hover:text-gray-500"
-          >
-            Console
-          </button>
-        </div>
+        {debugMode ? (
+          <div>
+            <button
+              onClick={() => {
+                navigateTo('/')
+              }}
+              className="font-bold py-2 px-4 rounded mx-1 border-solid border-2 border-black hover:border-gray-500 hover:bg-gray-200 hover:text-gray-500"
+            >
+              NFT ICA App
+            </button>
+            <button
+              onClick={() => {
+                navigateTo('/Console')
+              }}
+              className="font-bold py-2 px-4 rounded mx-1 border-solid border-2 border-black hover:border-gray-500 hover:bg-gray-200 hover:text-gray-500"
+            >
+              Console
+            </button>
+          </div>
+        ) : (
+          <h2 className="font-semibold text-lg">Interchain Account NFTs</h2>
+        )}
         <ConnectWallet />
       </div>
     </div>
